33d8d61a1bfa4cc18d558e8fe65f4bb8ed94e71f
Michael Fleig <michifleig@gmail.com>
2021-03-02 19:10:23 +0100

    purge css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: [
+        './public/**/*.html',
+        './src/**/*.vue',
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
